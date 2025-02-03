8be43738e1d63cc764ec72f8281e342bef24d581
Gaurav Pratap Singh <gs492979@gmail.com>
2025-02-04 03:34:21 +0530

    Sound added for keyboard key press & active btn animation.

--- index.js
+++ index.js
@@ -1,41 +1,64 @@
 let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 let audio = new Audio("./sounds/tom-1.mp3");
+
+//For button clicked
 for (let i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     let btnClicked = this.textContent;
-    switch (btnClicked) {
-      case "w":
-        let tom1 = new Audio("./sounds/tom-1.mp3");
-        tom1.play();
-
-        break;
-      case "a":
-        let tom2 = new Audio("./sounds/tom-2.mp3");
-        tom2.play();
-
-        break;
-      case "s":
-        let tom3 = new Audio("./sounds/tom-3.mp3");
-        tom3.play();
-        break;
-      case "d":
-        let tom4 = new Audio("./sounds/tom-4.mp3");
-        tom4.play();
-        break;
-      case "j":
-        let snare = new Audio("./sounds/snare.mp3");
-        snare.play();
-        break;
-      case "k":
-        let crash = new Audio("./sounds/crash.mp3");
-        crash.play();
-        break;
-      case "l":
-        let kickBass = new Audio("./sounds/kick-bass.mp3");
-        kickBass.play();
-        break;
-      default:
-        console.log(btnClicked);
-    }
+    makeSound(btnClicked);
+    buttonAnimation(btnClicked);
   });
 }
+
+//For key press on keyboard
+document.addEventListener("keydown", (e) => {
+  makeSound(e.key);
+  buttonAnimation(e.key);
+});
+
+const makeSound = (key) => {
+  switch (key) {
+    case "w":
+      let tom1 = new Audio("./sounds/tom-1.mp3");
+      tom1.play();
+
+      break;
+    case "a":
+      let tom2 = new Audio("./sounds/tom-2.mp3");
+      tom2.play();
+
+      break;
+    case "s":
+      let tom3 = new Audio("./sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      let tom4 = new Audio("./sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      let snare = new Audio("./sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      let crash = new Audio("./sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      let kickBass = new Audio("./sounds/kick-bass.mp3");
+      kickBass.play();
+      break;
+    default:
+      console.log(btnClicked);
+  }
+};
+
+//For button animation on mouse clicked and keyboard press
+
+const buttonAnimation = (currentKeyValue) => {
+  let activeBtn = document.querySelector(`.${currentKeyValue}`);
+  activeBtn.classList.add("pressed");
+  setTimeout(() => {
+    activeBtn.classList.remove("pressed");
+  }, 100);
+};
